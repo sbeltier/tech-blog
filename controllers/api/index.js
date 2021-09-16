4ecf560b8ef4e5dfd727220fa3a90daaa3b23481
sbeltier <sarahnbeltier@gmail.com>
2021-09-16 10:52:14 -0700

    need help with comment posting

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./userRoutes');
 const registrationRoutes = require('./registrationRoutes');
-
+const blogRoutes = require('./blogRoutes')
 
 router.use('/users', userRoutes);
 router.use('/register', registrationRoutes)
+router.use('/blog', blogRoutes)
 
 module.exports = router;
--- public/js/comment-script.js
+++ public/js/comment-script.js
@@ -2,18 +2,41 @@ const button_AddComment = document.getElementById('add-comment')
 const button_SubmitComment = document.getElementById('submit-button')
 const button_CancelComment = document.getElementById('cancel-button')
 
-button_AddComment.addEventListener('click', function (){
+button_AddComment.addEventListener('click', function () {
+    // Open Form
     const form = document.getElementById('form')
     form.setAttribute('style', 'display: show;')
-    console.log("button clicked")
+
 })
 
-button_SubmitComment.addEventListener('click', function (){
-    const form = document.getElementById('form')
-    form.setAttribute('style', 'display: none;')
+button_SubmitComment.addEventListener('click', function () {
+
+    const comment = document.getElementById('about').value
+    console.log('phase 1')
+    console.log(comment)
+
+    fetch('/api/blog/comment', {
+        method: "POST",
+        body: JSON.stringify({comment: comment}),
+        headers: { 'Content-Type': 'application/json' }
+
+    })
+
+        .then(function (response) {
+            console.log('phase 2')
+            if (response) {
+                // Close Form
+                const form = document.getElementById('form')
+                form.setAttribute('style', 'display: none;')
+            } else {
+            alert('Comment not submitted')
+        }
+        })
 })
 
-button_CancelComment.addEventListener('click', function (){
+button_CancelComment.addEventListener('click', function () {
+
+    // Close Form
     const form = document.getElementById('form')
     form.setAttribute('style', 'display: none;')
 })
